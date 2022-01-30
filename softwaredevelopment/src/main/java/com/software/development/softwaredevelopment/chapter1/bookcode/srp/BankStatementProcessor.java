0914c5d2f813d4ee3b1f6df35532dbf772c351d6
WooCheol <jikimee64@gmail.com>
2022-01-31 04:33:44 +0900

    [SD, Refactor] 2장 입출금 내역 분석기(책코드)
    1. 높은 응집도
    2. 낮은 결합도

---
+++ softwaredevelopment/src/main/java/com/software/development/softwaredevelopment/chapter1/bookcode/srp/BankStatementProcessor.java
@@ -0,0 +1,46 @@
+package com.software.development.softwaredevelopment.chapter1.bookcode.srp;
+
+import com.software.development.softwaredevelopment.chapter1.bookcode.dto.BankTransaction;
+import java.time.Month;
+import java.util.List;
+
+/**
+ * 계산 연산 그룹화(응집도 상승)
+ */
+public class BankStatementProcessor {
+
+    private final List<BankTransaction> bankTransactions;
+
+    public BankStatementProcessor(List<BankTransaction> bankTransactionList) {
+        this.bankTransactions = bankTransactionList;
+    }
+
+    public double calculateTotalAmount() {
+        double total = 0;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
+    }
+
+    public double calculateTotalInMonth(final Month month) {
+        double total = 0;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
+    }
+
+    public double calculateTotalForCategory(final String category) {
+        double total = 0;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDescription().equals(category)) {
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
+    }
+
+}

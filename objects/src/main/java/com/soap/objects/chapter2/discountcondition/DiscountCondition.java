dbf4e9dd3f3a11e13970daa0d1a44c0bd97bb8f4
WooCheol <jikimee64@gmail.com>
2022-03-16 22:38:27 +0900

    [Object, 1차복습] 더미코드 제거

--- objects/src/main/java/com/soap/objects/chapter2/discountcondition/DiscountCondition.java
+++
@@ -1,7 +0,0 @@
-package com.soap.objects.chapter2.discountcondition;
-
-import com.soap.objects.chapter2.Reservation;
-
-public interface DiscountCondition {
-    boolean checkCondition(Reservation reservation);
-}
--- objects/src/main/java/com/soap/objects/chapter2/discountpolicy/DiscountPolicy.java
+++
@@ -1,7 +0,0 @@
-package com.soap.objects.chapter2.discountpolicy;
-
-import com.soap.objects.chapter2.Reservation;
-
-public interface DiscountPolicy {
-    Reservation calculate(Reservation reservation);
-}
